b645fca1ee276cf684c1424b63936c716ce71d51
Samuel Gröner <samuel.groener@gmx.de>
2021-05-24 18:05:23 +0200

    rebased all former Sokoban functions to SokobanLevel for cleaner code. relocated the movement calls to Sokoban for cleaner code.

--- src/main/java/de/uulm/sp/pvs/sokoban/SokobanLevel.java
+++ src/main/java/de/uulm/sp/pvs/sokoban/SokobanLevel.java
@@ -1,6 +1,7 @@
 package de.uulm.sp.pvs.sokoban;
 
 
+import de.uulm.sp.pvs.exceptions.SokobanException;
 import de.uulm.sp.pvs.util.*;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
@@ -14,9 +15,16 @@ import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 
+/**
+ * @author Samuel Gröner
+ * implements the reading
+ *
+ * */
 public class SokobanLevel {
 
     public final List<String> authors;
@@ -26,7 +34,7 @@ public class SokobanLevel {
     public final int width;
     public final int height;
 
-    public SokobanLevel(String path) throws SAXException {
+    public SokobanLevel(String path) throws SokobanException {
 
          Schema schema;
          try {
@@ -34,7 +42,7 @@ public class SokobanLevel {
                      newSchema(new File("resources/sokoban.xsd"));
          }catch(SAXException saxException){
 
-             throw new SAXException("Error reading schema");
+             throw new SokobanException("Error reading schema",saxException);
          }
 
          DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
@@ -45,14 +53,14 @@ public class SokobanLevel {
         try{
             documentBuilder = documentBuilderFactory.newDocumentBuilder();
         }catch (ParserConfigurationException parserConfigurationException){
-            throw new SAXException("Error in parser configuration");
+            throw new SokobanException("Error in parser configuration",parserConfigurationException);
         }
 
         Document document;
         try{
             document = documentBuilder.parse(path);
-        }catch (IOException ioException){
-            throw new SAXException("Error while parsing file");
+        }catch (SAXException | IOException exception){
+            throw new SokobanException("Error while parsing file",exception);
         }
 
         NodeList nodeListAuthors = document.getElementsByTagName("Author");
@@ -81,11 +89,75 @@ public class SokobanLevel {
                 if (levelString[i].length() == width) {
                     level[i] = levelString[i].toCharArray();
                 }else{
-                    throw new SAXException("width is " + level[i].length + " but should be " + width);
+                    throw new SokobanException("width is " + level[i].length + " but should be " + width);
                 }
             }
         }else{
-            throw new SAXException("height is " + levelString.length + " but should be " + height);
+            throw new SokobanException("height is " + levelString.length + " but should be " + height);
         }
+    }
+    /**
+     * Finds the player in a sokoban play field
+     * @param array the play field
+     * @return the position of the player as a {@link Pair}
+     * */
+    public Pair<Integer,Integer> findPlayer(char[][] array){
+
+        int first = -1;
+        int second = -1;
+
+        for (char[] value:array) {
+            first++;
+            for (char obj:value) {
+                second++;
+                if (obj == '@') {
+                    return new Pair<>(first,second);
+                }
+            }
+            second = -1;
+        }
+        return new Pair<>(first,second);
+    }
+    /**
+     * Turns the play field into a String for output.
+     * @implNote replaces ", " with "" because Arrays.toString(char[] a)
+     *              puts them with ", " together
+     * @param array the play field
+     * @return the Sokoban play field as a String
+     * */
+    public String sokobanToString(char[][] array){
+        return Arrays.stream(array).map((char[] value)-> Arrays.toString(value).replace(", ","")).
+                collect(Collectors.joining("\n"));
+    }
+    /**
+     * Moves the player in the particular {@link Direction}
+     * @param array is the play field
+     * @param direction the direction parameters
+     * @return if player was able to move or not as boolean
+     * */
+    public boolean move(char[][] array, Direction direction){
+        //player cords:
+        Pair<Integer,Integer> pair = findPlayer(array);
+        int first = pair.getFirst();
+        int second = pair.getSecond();
+        //normal move
+        if (array[first + direction.first][second + direction.second] == ' ') {
+
+            array[first][second] = ' ';
+            array[first + direction.first][second + direction.second] = '@';
+            return true;
+        }
+        //move with push
+        if (array[first + direction.first][second + direction.second] == '$') {
+            if (array[first + (direction.first*2)][second + (direction.second*2)] == ' ') {
+
+                array[first][second] = ' ';
+                array[first + direction.first][second + direction.second] = '@';
+                array[first + (direction.first*2)][second + (direction.second*2)] = '$';
+                return true;
+            }
+        }
+        System.err.println("moving failed");
+        return false;
     }
 }
