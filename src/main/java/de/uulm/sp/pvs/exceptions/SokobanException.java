23e6ad86391494f934580c0e2d756f66e63a50f3
Samuel Gröner <samuel.groener@gmx.de>
2021-05-27 15:01:12 +0200

    general improvements in code an documentation

--- src/main/java/de/uulm/sp/pvs/exceptions/SokobanException.java
+++ src/main/java/de/uulm/sp/pvs/exceptions/SokobanException.java
@@ -1,5 +1,8 @@
 package de.uulm.sp.pvs.exceptions;
 
+/**
+ * crated a own exception so i dont have to use 100 different ones
+ * */
 public class SokobanException extends Exception{
 
     public SokobanException(String msg){super(msg);}
